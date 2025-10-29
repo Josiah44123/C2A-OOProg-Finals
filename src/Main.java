d8d872418571f824fbb5eb5c2bff947a2793eba9
Lance Derick De Villa <devillalance1025@gmail.com>
2025-10-29 22:20:41 +0800

    Create Main.java
    Main part of the Banking System

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+// Main.java
+import java.util.*;
+import java.io.*;
+import java.time.*;
+import java.time.format.*;
+
+public class Main {
+    public static void main(String[] args) {
+        BankSystem system = new BankSystem();
+        system.start();
+    }
+}
+
+//NOTE: Pagkakasunod-sunod (sana)
+//Abstract
+//Savings
+//Checking
+//Business
+//Transaction
+//Client
+//Admin
+//Main
