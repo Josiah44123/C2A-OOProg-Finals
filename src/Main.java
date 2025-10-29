c39df865a5fa3bc9dfba453095025c3426e6c998
Lance Derick De Villa <devillalance1025@gmail.com>
2025-10-29 22:22:04 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -20,3 +20,15 @@ public class Main {
 //Client
 //Admin
 //Main
+
+//Expected Output 
+// ===========================================
+//    WELCOME TO ADVANCED BANKING SYSTEM
+// ===========================================
+
+// --- MAIN MENU ---
+// 1. Register New Client
+// 2. Client Login
+// 3. Admin Login
+// 4. Exit
+// Choose option: 
