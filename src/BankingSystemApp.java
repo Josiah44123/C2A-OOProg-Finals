7da82738a2e9da019ffba9837191d6ea61b5419e
Nicko <cjnickorosales@gmail.com>
2025-11-03 13:16:20 +0800

    Addition to Main

--- src/BankingSystemApp.java
+++ src/BankingSystemApp.java
@@ -9,16 +9,55 @@ public class BankingSystemApp {
     
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
-        
+        Bank bank = new Bank("Bank-System");
+
    
        
         System.out.println("╔════════════════════════════════════════╗");
         System.out.println("║   WELCOME TO ADVANCED BANKING SYSTEM   ║");
         System.out.println("╚════════════════════════════════════════╝");
-        
-        
-        scanner.close();
+
+        while (true) {
+            char option = displayMainMenu(scanner);
+
+            switch (option) {
+                case '1' : {registerNewClient(); break;}
+                case '2' : 
+                case '3' :
+                case '4' : System.exit(0);
+                default : break;
+            }
+        }
     }
+
+    public static char displayMainMenu(Scanner scanner){
+        System.out.println("--- MAIN MENU ---");
+        System.out.println("1. Register New Client");
+        System.out.println("2. Client Login");
+        System.out.println("3. Admin Login");
+        System.out.println("4. Exit");
+        System.out.print("Choose an option: ");
+        char option = scanner.next().charAt(0);
+
+        return option;
+    }
+
+    public static void registerNewClient(){ //lagyan regex
+        String name;
+        String password;
+        int pin;
+
+        System.out.println("--- REGISTER NEW CLIENT ---");
+        System.out.print("Enter Client Name: ");
+        name = scanner.nextLine();
+        System.out.print("Enter Password: ");
+        password = scanner.nextLine();
+        System.out.print("Enter 4-digit Pin: ");
+        pin = scanner.nextInt(); 
+
+        bank.createClient(name, password, pin);
+    }
+
 }
 
 //NOTE: Pagkakasunod-sunod (sana)
