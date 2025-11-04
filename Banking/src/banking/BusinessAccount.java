d9868d561cc834b69744a426ab7a3aa75716fd7d
Josiah44123 <163369246+Josiah44123@users.noreply.github.com>
2025-11-05 05:20:20 +0800

    skeleton BusinessAccount

--- Banking/src/banking/BusinessAccount.java
+++ Banking/src/banking/BusinessAccount.java
@@ -1,5 +1,42 @@
 package banking;
 
-public class BusinessAccount {
+public class BusinessAccount extends Account {
+    private static final double INTEREST_RATE = 0.02; // 2% annual
+    private static final double SERVICE_FEE = 25.0;
+    private static final double TRANSACTION_LIMIT = 100000.0; // Daily transaction limit
     
+    public BusinessAccount(String clientId, double initialDeposit) {
+        super(clientId, initialDeposit);
+    }
+    
+    @Override
+    public boolean transfer(Account targetAccount, double amount) {
+        
+        return; 
+    }
+    
+    @Override
+    public void applyInterest() {
+  
+    }
+    
+    @Override
+    public void applyServiceFee() {
+        
+    }
+    
+    @Override
+    public String getAccountType() {
+        return "BUSINESS";
+    }
+    
+    @Override
+    public double getInterestRate() {
+        return INTEREST_RATE;
+    }
+    
+    @Override
+    public double getServiceFee() {
+        return SERVICE_FEE;
+    }
 }
