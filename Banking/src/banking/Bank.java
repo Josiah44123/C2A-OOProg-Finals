ce1dec7790c971bd398d532190d8e5f4b6db8d2d
Josiah44123 <163369246+Josiah44123@users.noreply.github.com>
2025-11-05 05:17:12 +0800

    Bank (code from Miguel)

--- Banking/src/banking/Bank.java
+++ Banking/src/banking/Bank.java
@@ -1,41 +1,108 @@
 package banking;
 import java.io.*;
 import java.util.*;
-
-public class Bank implements java.io.Serializable {
-    private static final long serialVersionUID = 1L;
-    private static final String DATA_FILE = "bank_data.ser";
+public class Bank {
     private static final String ADMIN_PASSWORD = "admin123";
-
+    
     private String bankName;
     private Map<String, Client> clients;
     private Map<String, Account> accounts;
-
+    
     public Bank(String bankName) {
         this.bankName = bankName;
         this.clients = new HashMap<>();
         this.accounts = new HashMap<>();
     }
-public Client createClient(String name, String password, int pin) {
-        Client client = new Client(name, password, pin);
+    
+    // Client Management
+    public Client createClient(String name, String pin) {
+        Client client = new Client(name, pin);
         clients.put(client.getClientId(), client);
         return client;
     }
-
-public Account createAccount(String clientId, String accountType, double initialDeposit) {
-        Client client = getClientById(clientId);
+    
+    public Client getClient(String clientId) {
+        return clients.get(clientId);
+    }
+    
+    public Client findClientByName(String name) {
+        for (Client client : clients.values()) {
+            if (client.getName().equalsIgnoreCase(name)) {
+                return client;
+            }
+        }
+        return null;
+    }
+    
+    // Account Management
+    public Account createAccount(String clientId, String accountType, double initialDeposit) {
+        Client client = clients.get(clientId);
         if (client == null || initialDeposit < 0) {
             return null;
         }
-      Account account;
-      if (accountType.equalsIgnoreCase("Savings")) {
-          account = new SavingsAccount(clientId, initialDeposit);
-      } else if (accountType.equalsIgnoreCase("Checking")) {
-          account = new CheckingAccount(clientId, initialDeposit);
-      } else {
-          return null;
-      }
-        accounts.put(account);
+        
+        Account account;
+        if (accountType.equalsIgnoreCase("SAVINGS")) {
+            account = new SavingsAccount(clientId, initialDeposit);
+        } else if (accountType.equalsIgnoreCase("CHECKING")) {
+            account = new CheckingAccount(clientId, initialDeposit);
+        } else if (accountType.equalsIgnoreCase("BUSINESS")) {
+            account = new BusinessAccount(clientId, initialDeposit);
+        } else {
+            return null;
+        }
+        
+        accounts.put(account.getAccountId(), account);
         client.addAccount(account.getAccountId());
         return account;
+    }
+    
+    public Account getAccount(String accountId) {
+        return accounts.get(accountId);
+    }
+    
+    public List<Account> getClientAccounts(String clientId) {
+        List<Account> clientAccounts = new ArrayList<>();
+        Client client = clients.get(clientId);
+        if (client != null) {
+            for (String accountId : client.getAccountIds()) {
+                Account account = accounts.get(accountId);
+                if (account != null) {
+                    clientAccounts.add(account);
+                }
+            }
+        }
+        return clientAccounts;
+    }
+    
+    // Monthly Operations
+    public void applyMonthlyInterestAndFees() {
+        for (Account account : accounts.values()) {
+            account.applyInterest();
+            account.applyServiceFee();
+        }
+    }
+    
+    // Admin Functions
+    public boolean authenticateAdmin(String password) {
+        return ADMIN_PASSWORD.equals(password);
+    }
+    
+    public Collection<Client> getAllClients() {
+        return new ArrayList<>(clients.values());
+    }
+    
+    public Collection<Account> getAllAccounts() {
+        return new ArrayList<>(accounts.values());
+    }
+    
+    public double getTotalBankBalance() {
+        return accounts.values().stream()
+                .mapToDouble(Account::getBalance)
+                .sum();
+    }
+    
+    public String getBankName() {
+        return bankName;
+    }
 }
